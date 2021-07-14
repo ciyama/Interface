f061515f5443356aa14aafba9cf5ffebd1d329b6
Celio Iyama <celio.iyama@gmail.com>
2021-07-14 09:35:07 -0300

    Area Circle and Rectangle Abstract Class mode - java

--- abstractClass/src/model/entities/Shape.java
+++ abstractClass/src/model/entities/Shape.java
@@ -1,25 +1,7 @@
 package model.entities;
 
-import model.enums.Color;
-
-public abstract class Shape {
-	
-	private Color color;
-
-	public Shape(Color color) {
-		this.color = color;
-	}
-
-	public Color getColor() {
-		return color;
-	}
-
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	
-	public abstract double area();
-	
-	
-
+public interface Shape {
+		
+	//public abstract double area(); // nao tem necessidade do public abstract pois no metodo ele já é reconhecido como padrao
+	double area();
 }
